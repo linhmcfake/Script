ccbf33ca62c5115d0ea787e463930d97a94ed259
linhmcfake <linhmcfake@gmail.com>
2026-02-13 19:19:30 +0700

    Update loader script URLs in MaxNo1.lua

--- MaxNo1.lua
+++ MaxNo1.lua
@@ -1,2 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/linhmcfake/Script/refs/heads/main/REALBUILDANDBATTLEHUB.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/linhmcfake/Script/refs/heads/main/04b129bc934a4519.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/linhmcfake/Script/refs/heads/main/revoker"))()
 -- loader
