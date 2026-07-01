ec78f5a8790121c984063dde1e990a48f007dccd
linhmcfake <linhmcfake@gmail.com>
2026-07-01 18:55:59 +0700

    Update MaxNo1.lua

--- MaxNo1.lua
+++ MaxNo1.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/linhmcfake/Script/refs/heads/main/04b129bc934a4519.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/linhmcfake/Script/refs/heads/main/BuildAndBattle.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/linhmcfake/Script/refs/heads/main/revoker"))()
 -- loader
